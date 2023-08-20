440abd8bcbf4634d8fd403cbd1a5d57e8b882f90
Sujith-sunny <sunny.perikala@gmail.com>
2023-08-21 01:11:43 +0530

    Python: Added Loops

---
+++ Loops/ifelse.py
@@ -0,0 +1,14 @@
+x = 10
+name = 'Sujith'
+Exam = True
+MyList = []
+
+if x == 10:
+    print('X value is 10')
+else:
+    print('X value is not 10')
+
+if Exam:
+    print('I passed the exam')
+else: 
+    print('I failed the exam')
